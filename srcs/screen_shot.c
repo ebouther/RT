efd7fbedf9be785966029b0135ec5e814e4f4bfe
Adrien SCHOLLER <ascholle@e2r4p3.42.fr>
2016-04-26 12:07:50 +0200

    screen P3 to P6

--- srcs/screen_shot.c
+++ srcs/screen_shot.c
@@ -6,7 +6,7 @@
 /*   By: ascholle <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/04/24 18:01:02 by ascholle          #+#    #+#             */
-/*   Updated: 2016/04/25 16:41:07 by ascholle         ###   ########.fr       */
+/*   Updated: 2016/04/26 12:06:58 by ascholle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,44 +19,37 @@ void	ft_make_screen(t_env *e, char *name)
 	int				j;
 	int				k;
 	unsigned char	*p;
-	char			*c;
+	char			c;
 
 	if ((fd = open(name, O_WRONLY | O_CREAT)) == -1)
 		exit(-1);
-	write(fd, "P3\n", 3);
+	write(fd, "P6\n", 3);
 	i = SIZE_W;
 	j = SIZE_H;
 	ft_putnbr_fd(i, fd);
 	write(fd, " ", 1);
 	ft_putnbr_fd(j, fd);
-	write(fd, "\n255", 4);
+	write(fd, "\n255\n", 5);
 	(void)e;
 	i = 0;
 	p = (unsigned char*)(e->data);
 	while (i < SIZE_H)
 	{
-		write(fd, "\n", 1);
 		j = 0;
 		while (j < SIZE_W)
 		{
 			p += 2;
 			k = *p;
-			c = ft_itoa(k);
-			write(fd, c, ft_strlen(c));
-			free(c);
-			write(fd, " ", 1);
+			c = k;
+			write(fd, &c, 1);
 			p--;
 			k = *p;
-			c = ft_itoa(k);
-			write(fd, c, ft_strlen(c));
-			free(c);
-			write(fd, " ", 1);
+			c = k;
+			write(fd, &c, 1);
 			p--;
 			k = *p;
-			c = ft_itoa(k);
-			write(fd, c, ft_strlen(c));
-			free(c);
-			write(fd, " ", 1);
+			c = k;
+			write(fd, &c, 1);
 			p += 4;
 			j++;
 		}
